8ea7f0dc9fd96a8a1312f03da9855e3a5782495a
Omer Arzi <arziomer@gmail.com>
2022-02-08 11:16:26 +0200

    DOM updated

--- index.js
+++ index.js
@@ -1,9 +1,62 @@
+const mainEl = document.querySelector('main')
+initChannels(mainEl)
 const channels = document.querySelectorAll('.channel-container');
 const audios = document.querySelectorAll('audio');
 const mutesButtons = document.querySelectorAll('.channel-container .switch input')
 const playAll = document.getElementById('play');
 const pauseAll = document.getElementById('stop')
 const loop = document.getElementById('loop')
+
+
+function createChannel(name, audioSourcePath){
+    const sectionEl = document.createElement("section")
+    const divName = document.createElement("div")
+    const labelEl = document.createElement("label")
+    const labelElInput = document.createElement("input")
+    const span =document.createElement("span")
+    const audioDiv = document.createElement("div")
+    const audioEl = document.createElement("audio")
+    const audioSource = document.createElement("source")
+
+    sectionEl.classList.add("channel-container")
+    divName.classList.add("channel-name")
+    divName.innerHTML = name
+    labelEl.classList.add("switch")
+    labelElInput.setAttribute("type", "checkbox")
+    span.classList.add("slider", "round")
+    audioDiv.classList.add("audio-container")
+    audioSource.src = audioSourcePath
+    audioSource.type = "audio/mpeg"
+
+    audioEl.appendChild(audioSource)
+    audioDiv.appendChild(audioEl)
+    labelEl.appendChild(labelElInput)
+    labelEl.appendChild(span)
+    sectionEl.appendChild(divName)
+    sectionEl.appendChild(labelEl)
+    sectionEl.appendChild(audioDiv)
+
+    return sectionEl
+}
+
+function initChannels(){
+    const channelDict = {
+        Bass:"/audio/Bass.mp3",
+        Kick: "/audio/Kick.mp3",
+        Snare: "/audio/Snare.mp3",
+        Piano: "/audio/Piano.mp3",
+        Guitar: "/audio/Guitar.mp3",
+        Lead: "/audio/Lead.mp3",
+        Brass: "/audio/Brass.mp3",
+        Synth: "/audio/Synth.mp3"
+    }
+
+    for(const[name,audioPath] of Object.entries(channelDict)){
+        const currSection = createChannel(name, audioPath)
+        mainEl.appendChild(currSection)
+        console.log(currSection.innerHTML)
+    }
+}
 
 function colorizeElements(elements) {
     let hue = 0
